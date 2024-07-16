47fc65359453902e9b60c0d44c4bdc036ebe4891
Md Farhad Hossen Imon <farhadimon8@gmail.com>
2024-07-17 04:28:16 +0600

    S
    Assignment 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,248 +1,53 @@
-// Current SDK version: 3.22.1
 import 'package:flutter/material.dart';
 
-// TextField
-
 void main() {
-  runApp(IntroApp());
+  runApp(MyApp());
 }
 
-class IntroApp extends StatelessWidget {
-  const IntroApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-      title: 'Intro app',
-      theme: ThemeData(
-          brightness: Brightness.light,
-          appBarTheme: AppBarTheme(
-              backgroundColor: Colors.pink,
-              titleTextStyle: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: FontWeight.w700,
-                  letterSpacing: 1.2
-              )
-          ),
-          textButtonTheme: TextButtonThemeData(
-              style: TextButton.styleFrom(
-                  foregroundColor: Colors.pink,
-                  textStyle: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700
-                  )
-              )
-          ),
-          inputDecorationTheme: InputDecorationTheme(
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: BorderSide(
-                    color: Colors.green,
-                    width: 2
-                )
-            ),
-            disabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: BorderSide(
-                    color: Colors.green,
-                    width: 2
-                )
-            ),
-            focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: BorderSide(
-                    color: Colors.blueAccent,
-                    width: 2
-                )
-            ),
-            labelStyle: TextStyle(
-                fontSize: 16
-            ),
-            hintStyle: TextStyle(
-                color: Colors.purple.shade200
-            ),
-          )
-      ),
-      darkTheme: ThemeData(
-          brightness: Brightness.dark,
-          appBarTheme: AppBarTheme(
-              backgroundColor: Colors.green,
-              titleTextStyle: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: FontWeight.w700,
-                  letterSpacing: 1.2
-              )
-          ),
-          textButtonTheme: TextButtonThemeData(
-              style: TextButton.styleFrom(
-                  foregroundColor: Colors.green,
-                  textStyle: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700
-                  )
-              )
-          ),
-          inputDecorationTheme: InputDecorationTheme(
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: BorderSide(
-                    color: Colors.green,
-                    width: 2
-                )
-            ),
-            disabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: BorderSide(
-                    color: Colors.green,
-                    width: 2
-                )
-            ),
-            focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8),
-                borderSide: BorderSide(
-                    color: Colors.blueAccent,
-                    width: 2
-                )
-            ),
-            labelStyle: TextStyle(
-                fontSize: 16
-            ),
-            hintStyle: TextStyle(
-                color: Colors.purple.shade200
-            ),
-          )
-      ),
-      themeMode: ThemeMode.dark,
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  TextEditingController _descriptionTEController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  return MaterialApp(
+    title: 'Greeting App',
+    home: Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
-        // backgroundColor: Colors.blue,
+        title: Text('Greeting App'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                enabled: true,
-                decoration: InputDecoration(
-                  label: Text('Name'),
-                  labelStyle: TextStyle(
-                      fontSize: 16
-                  ),
-                  hintText: 'Enter your name',
-                  hintStyle: TextStyle(
-                      color: Colors.green.shade200
-                  ),
-                  icon: Icon(Icons.add),
-                  prefixIcon: Icon(Icons.search),
-                  suffixIcon: TextButton(
-                    child: Text('Search'),
-                    onPressed: () {},
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide(
-                          color: Colors.green,
-                          width: 2
-                      )
-                  ),
-                  disabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide(
-                          color: Colors.green,
-                          width: 2
-                      )
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                      borderSide: BorderSide(
-                          color: Colors.blueAccent,
-                          width: 2
-                      )
-                  ),
-                ),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                controller: _descriptionTEController,
-                enabled: true,
-                maxLines: 5,
-                maxLength: 200,
-                onTap: () {
-                  print('Tapped on textfield');
-                },
-                onChanged: (String value) {
-                  print(value);
-                },
-                decoration: InputDecoration(
-                  label: Text('Description'),
-                  labelStyle: TextStyle(
-                      fontSize: 16
-                  ),
-                  hintText: 'Enter your description',
-                ),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                enabled: true,
-                onChanged: (String value) {
-                  print(value);
-                },
-                obscureText: true,
-                controller: TextEditingController(),
-                decoration: InputDecoration(
-                  label: Text('Password'),
-                  labelStyle: TextStyle(
-                      fontSize: 16
-                  ),
-                  hintText: 'Enter your password',
-                ),
-              ),
-              SizedBox(height: 16),
-              TextField(),
-              TextButton(onPressed: () {
-                // _descriptionTEController.text = 'clear';
-                _descriptionTEController.clear();
-              }, child: Text('Clear description'))
-            ],
+      body: Center(
+        child: MyHomePage(),),
+    ),
+  );
+}
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Hello, World!',
+          style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+        ),
+        Text('Welcome to Flutter!'),
+        Image.asset('Assets/icons/Logo.jpg'),
+    ElevatedButton(
+          onPressed: () {
+            final snackBar = SnackBar(content: Text('Button Pressed!'));
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          },
+          child: Text('Press Me'),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
+      ],
     );
   }
 }
